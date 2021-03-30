2b90f27bb2cc71045da3dfb3db5e7479d62ed1a3
wtaylor17 <wtaylor@upei.ca>
2021-03-30 14:40:49 -0300

    Updated HAR eval

--- experiments/har/eval.py
+++ experiments/har/eval.py
@@ -4,14 +4,18 @@ from vprnn.models import load_vprnn
 import sys
 import numpy as np
 
-np.random.seed(12345)
 
+np.random.seed(12345)
 
 model = load_vprnn(sys.argv[1])
 model.compile(loss='binary_crossentropy',
               optimizer='sgd',
               metrics=['acc'])
-(x_train, y_train), (x_test, y_test) = load_har_stash()
+
+if len(sys.argv) == 2:
+    (x_train, y_train), (x_test, y_test) = load_har_stash()
+else:
+    (x_train, y_train), (x_test, y_test) = load_har_stash(sys.argv[2])
 mu = np.mean(x_train, axis=(0, 1)).reshape((1, 1, -1))
 std = np.std(x_train, axis=(0, 1)).reshape((1, 1, -1))
 x_train = (x_train - mu) / std
